41781847997ee3270d08c2d85ca1c4171710caa1
dahee oh <ekgml4122@gmail.com>
2023-08-16 20:02:30 +0900

    재귀순열, 조합, 중복조합

--- PCCP/재귀순열.py
+++ PCCP/재귀순열.py
@@ -1,3 +1,4 @@
+# 재귀 순열
 arr = ['A', 'B', 'C']  # 재료 리스트
 sel = [0, 0, 0]
 check = [0, 0, 0]
@@ -14,3 +15,15 @@ def perm(depth):
             perm(depth+1)
             check[i] = 0
 perm(0)
+
+# 조합 2개 뽑기
+arr = ['A', 'B', 'C']
+for i in range(3):
+    for j in range(i+1, 3):
+        print(arr[i], arr[j])
+
+# 중복 조합 2개 뽑기
+arr = ['A', 'B', 'C']
+for i in range(3):
+    for j in range(i, 3):
+        print(arr[i], arr[j])
