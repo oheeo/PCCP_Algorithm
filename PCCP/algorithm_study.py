b9075172996056a70bc7ee8599da41daa0153b7b
dahee oh <ekgml4122@gmail.com>
2023-08-23 22:39:55 +0900

    파이썬 입출력

--- PCCP/algorithm_study.py
+++ PCCP/algorithm_study.py
@@ -2,6 +2,15 @@
 # nums = input('입력해주세요: ')
 # print(nums, type(nums))
 
-nums = list(map(int, input().split()))
-print(nums, type(nums))
+# nums = list(map(int, input().split()))
+# print(nums, type(nums))
 # 1 2 3 입력시 [1, 2, 3] 출력
+
+# 2차원 리스트 받기
+nums_matrix = [list(map(int, input().split())) for _ in range(3)]
+print(nums_matrix, type(nums_matrix))
+# 1 2 3
+# 4 5 6
+# 7 8 9
+# 입력시 [[1, 2, 3], [4, 5, 6], [7, 8, 9]] <class 'list'> 출력
+
