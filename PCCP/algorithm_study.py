1636e57e14165ab4e53b6acc920a15aa3cf22a59
dahee oh <ekgml4122@gmail.com>
2023-08-26 23:13:32 +0900

    함수

--- PCCP/algorithm_study.py
+++ PCCP/algorithm_study.py
@@ -7,12 +7,18 @@
 # 1 2 3 입력시 [1, 2, 3] 출력
 
 # 2차원 리스트 받기
-nums_matrix = [list(map(int, input().split())) for _ in range(3)]
-print(nums_matrix, type(nums_matrix))
+# nums_matrix = [list(map(int, input().split())) for _ in range(3)]
+# print(nums_matrix, type(nums_matrix))
 # 1 2 3
 # 4 5 6
 # 7 8 9
 # 입력시 [[1, 2, 3], [4, 5, 6], [7, 8, 9]] <class 'list'> 출력
 
 
-# 함수와 재귀함수
+# 함수
+def some_func(param1, param2):
+    return [param1, param2]
+
+print(some_func(1, 2))  # [1, 2]
+
+# 재귀함수
