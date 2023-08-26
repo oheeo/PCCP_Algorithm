a2dac7e9b4ebbc35c6621dc806d145178c78a5f3
dahee oh <ekgml4122@gmail.com>
2023-08-26 23:18:19 +0900

    메모이제이션

--- PCCP/algorithm_study.py
+++ PCCP/algorithm_study.py
@@ -28,5 +28,12 @@ def fibo(n):
         return n
     else:
         return fibo(n-1) + fibo(n-2)  # 재귀 호출
-    
+print(fibo(10))  # 55
+
+# 참고(효율적인 메모이제이션)
+memo = [0, 1]
+def fibo(n):
+    if n >= 2 and n >= len(memo):
+        memo.append(fibo(n-1) + fibo(n-2))
+    return memo[n]
 print(fibo(10))  # 55
