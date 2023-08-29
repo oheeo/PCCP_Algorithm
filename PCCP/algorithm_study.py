83251b1cd1e4eb3608f9260514114528e73a7636
dahee oh <ekgml4122@gmail.com>
2023-08-30 08:32:35 +0900

    스트링 알고리즘-문자열 포함 여부 검사

--- PCCP/algorithm_study.py
+++ PCCP/algorithm_study.py
@@ -146,6 +146,8 @@ print("-----------------------------------------")
 
 
 # 스트링 알고리즘
+
+
 # 파이써닉 코드
 # word = input('단어를 입력하세요: ')
 
@@ -169,19 +171,43 @@ print("-----------------------------------------")
 
 
 # two pointer 활용
-word = input('단어를 입력하세요: ')
+# word = input('단어를 입력하세요: ')
 
-left = 0 # 시작 인덱스
-right = len(word)-1 # 마지막 인덱스
+# left = 0 # 시작 인덱스
+# right = len(word)-1 # 마지막 인덱스
 
-is_palindrome = True
-while left < right:  # 왼쪽과 오른쪽이 유지될때까지 돌아라!
-    if word[left] == word[right]:  # 같은 경우라면 아직은 회문 조건을 만족함
-        left += 1  # 왼쪽 포인터를 한칸 오른쪽으로 증가시키고
-        right -= 1  # 오른쪽 포인터를 한 칸 왼쪽으로 감소
-        continue
-    else:  # 다른 경우라면 그 즉시 안된다고 표시하고 break
-        is_palindrome = False
-        break
+# is_palindrome = True
+# while left < right:  # 왼쪽과 오른쪽이 유지될때까지 돌아라!
+#     if word[left] == word[right]:  # 같은 경우라면 아직은 회문 조건을 만족함
+#         left += 1  # 왼쪽 포인터를 한칸 오른쪽으로 증가시키고
+#         right -= 1  # 오른쪽 포인터를 한 칸 왼쪽으로 감소
+#         continue
+#     else:  # 다른 경우라면 그 즉시 안된다고 표시하고 break
+#         is_palindrome = False
+#         break
 
-print(is_palindrome)
+# print(is_palindrome)
+
+
+# 문자열 포함 여부 검사하기
+t = 'hello world' # text
+p = 'wor'         # target pattern
+
+def find_word(p, t):
+    N = len(t)
+    M = len(p)
+
+    for i in range(N-M+1):
+        cnt = 0
+        for j in range(M):
+            if t[i+j] == p[j]:
+                cnt += 1
+            else:
+                break
+
+        if cnt == M:
+            return i
+
+    return '못찾았음'
+
+print(find_word(p, t))
