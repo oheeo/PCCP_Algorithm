5af7f1ff529dd6fe1ee8d35248855929974254a2
dahee oh <ekgml4122@gmail.com>
2023-08-30 08:29:51 +0900

    스트링 알고리즘

--- PCCP/algorithm_study.py
+++ PCCP/algorithm_study.py
@@ -156,13 +156,32 @@ print("-----------------------------------------")
 
 
 # 반복문 사용
+# word = input('단어를 입력하세요: ')
+
+# reversed_word = ''
+# for i in word:
+#     reversed_word = i + reversed_word
+
+# if word == reversed_word:
+#     print('회문입니다')
+# else:
+#     print('회문이 아닙니다')
+
+
+# two pointer 활용
 word = input('단어를 입력하세요: ')
 
-reversed_word = ''
-for i in word:
-    reversed_word = i + reversed_word
+left = 0 # 시작 인덱스
+right = len(word)-1 # 마지막 인덱스
 
-if word == reversed_word:
-    print('회문입니다')
-else:
-    print('회문이 아닙니다')
+is_palindrome = True
+while left < right:  # 왼쪽과 오른쪽이 유지될때까지 돌아라!
+    if word[left] == word[right]:  # 같은 경우라면 아직은 회문 조건을 만족함
+        left += 1  # 왼쪽 포인터를 한칸 오른쪽으로 증가시키고
+        right -= 1  # 오른쪽 포인터를 한 칸 왼쪽으로 감소
+        continue
+    else:  # 다른 경우라면 그 즉시 안된다고 표시하고 break
+        is_palindrome = False
+        break
+
+print(is_palindrome)
