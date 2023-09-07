42176801a6746155e3688b2bf43cbb83e2f9aee3
dahee oh <ekgml4122@gmail.com>
2023-09-07 23:09:22 +0900

    DFS 스택+인접행렬

--- PCCP/algorithm_study.py
+++ PCCP/algorithm_study.py
@@ -457,3 +457,32 @@ visited = [1, 3, 7]
 stack = [2, 6]
 
 # 이런식으로 2는 계속 기다리고, 3 -> 7 -> 6 식으로 쭉쭉 "깊게" 뻗어나감!
+
+
+# DFS 풀이법
+# 스택 + 인접 행렬
+V, E = map(int, input().split())  # Vertex, Edge 갯수
+
+adj_matrix = [[0] * (V + 1) for _ in range(V + 1)]  # 인접행렬 기본틀
+
+for _ in range(E):  # 간선 갯수만큼 돌면서 연결 정보를 받음
+    start, end = map(int, input().split())  # 시작점과 끝점
+    adj_matrix[start][end] = 1
+    adj_matrix[end][start] = 1  # 양방향 그래프니까!!
+
+stack = [1]  # 맨처음 시작점은 1번 포도알
+visited = []  # 궤적 기록용
+
+while stack:  # 스택이 빌때까지 돌아라!
+    current = stack.pop()  # 우선 스택에서 현재 위치 하나 뽑고
+    if current not in visited:  # 방문하지 않은 곳이라면,
+        visited.append(current)  # 방문했다고 체크해줌
+		
+		# 위의 if문 안으로 넣으면 더 좋습니다.
+    for destination in range(V+1):  # current 입장에서 어디로 갈 수 있는지 모조리 체크
+        if adj_matrix[current][destination] and destination not in visited:  # 갈수있고 + 방문 안했으면!
+            stack.append(destination)  # 다음 갈 곳으로 Stack에 저장
+
+print('이동경로:', *visited)
+
+# 이동경로: 1 3 7 6 5 2 4
