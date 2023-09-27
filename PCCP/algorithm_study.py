5499daa1aa49b60b9520b27ec4c490f95e46d702
dahee oh <ekgml4122@gmail.com>
2023-09-28 03:41:26 +0900

    BFS 너비우선탐색

--- PCCP/algorithm_study.py
+++ PCCP/algorithm_study.py
@@ -673,3 +673,8 @@ print(people) # ['jane', 'bob', 'brad', 'kelly']
 # Queue를 활용하여 BFS를 구현할 수 있다.
 V, E = map(int, input().split())  # Vertex, Edge 갯수
 adj_matrix = [[0] * (V + 1) for _ in range(V + 1)]  # 인접행렬 기본틀
+
+for _ in range(E):  # 간선 갯수만큼 돌면서 연결 정보를 받음
+    start, end = map(int, input().split())  # 시작점과 끝점
+    adj_matrix[start][end] = 1
+    adj_matrix[end][start] = 1  # 양방향 그래프니까!!
